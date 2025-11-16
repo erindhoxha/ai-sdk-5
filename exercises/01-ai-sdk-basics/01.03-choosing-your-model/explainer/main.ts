df1fb8d6c34dcfb96fef563e92e0ca08da6c98d2
erindhoxha <hello@erindhoxha.dev>
2025-11-16 18:00:41 +0100

    Use 4o mini

--- exercises/01-ai-sdk-basics/01.03-choosing-your-model/explainer/main.ts
+++ exercises/01-ai-sdk-basics/01.03-choosing-your-model/explainer/main.ts
@@ -7,6 +7,6 @@ import { google } from '@ai-sdk/google';
 // Requires an ANTHROPIC_API_KEY environment variable in .env
 import { anthropic } from '@ai-sdk/anthropic';
 
-const model = openai('gpt-4o2-mini');
+const model = openai('gpt-4o-mini');
 
 console.dir(model, { depth: null });
